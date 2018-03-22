79cbbb5addd9242057b94b33de05bbfdf0b23d5d
riordhn <ramadhanar.rio@gmail.com>
2018-03-22 12:18:11 +0700

    refix closed

---
+++ resources/views/layouts/closed.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<title>Website ditutup sementara</title>
+<style>
+  body { text-align: center; padding: 150px; }
+  h1 { font-size: 50px; }
+  body { font: 20px Helvetica, sans-serif; color: #333; }
+  article { display: block; text-align: left; width: 650px; margin: 0 auto; }
+  a { color: #dc8100; text-decoration: none; }
+  a:hover { color: #333; text-decoration: none; }
+</style>
+
+<article>
+    <h1>Pendaftaran dan pengisian berkas telah selesai</h1>
+    <div>
+        <p>Pendaftaran dan pengisian berkas telah melewati waktu yang ditentukan, mohon tunggu info selanjutnya. Berkas-berkas Anda dalam pengecekan kami.</p>
+        <p>&mdash; The Team</p>
+    </div>
+</article>
--- routes/web.php
+++ routes/web.php
@@ -10,20 +10,23 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', ['uses' => 'LayoutController@indexHome']);
-Route::get('pengumuman-berkas', ['uses' => 'LayoutController@indexPengumuman']);
-Route::get('contact-person', ['uses' => 'LayoutController@indexContact']);
-Route::get('daftar', ['uses' => 'LayoutController@indexDaftar']);
-Route::get('upload-berkas', ['uses' => 'LayoutController@indexUploadBerkas']);
-
-Route::post('daftar', ['uses' => 'FunctionController@actionPelamarDaftar']);
-Route::post('signin', ['uses' => 'FunctionController@actionPelamarLogin']);
-
-Route::group(['middleware' => ['auth']], function () {
-    Route::get('signout', ['uses' => 'FunctionController@actionPelamarLogout']);
-    Route::post('upload-berkas', ['uses' => 'FunctionController@actionPelamarUploadBerkas']);
-    Route::post('finish-upload', ['uses' => 'FunctionController@actionPelamarFinishUpload']);
-    Route::get('ubah-password', ['uses' => 'LayoutController@indexUbahPassword']);
-    Route::post('ubah-password', ['uses' => 'FunctionController@actionUbahPassword']);
+Route::get('/', function(){
+    return view('layouts/closed');
 });
+
+// Route::get('/', ['uses' => 'LayoutController@indexHome']);
+// Route::get('pengumuman-berkas', ['uses' => 'LayoutController@indexPengumuman']);
+// Route::get('contact-person', ['uses' => 'LayoutController@indexContact']);
+// Route::get('daftar', ['uses' => 'LayoutController@indexDaftar']);
+// Route::get('upload-berkas', ['uses' => 'LayoutController@indexUploadBerkas']);
+
+// Route::post('daftar', ['uses' => 'FunctionController@actionPelamarDaftar']);
+// Route::post('signin', ['uses' => 'FunctionController@actionPelamarLogin']);
+
+// Route::group(['middleware' => ['auth']], function () {
+//     Route::get('signout', ['uses' => 'FunctionController@actionPelamarLogout']);
+//     Route::post('upload-berkas', ['uses' => 'FunctionController@actionPelamarUploadBerkas']);
+//     Route::post('finish-upload', ['uses' => 'FunctionController@actionPelamarFinishUpload']);
+//     Route::get('ubah-password', ['uses' => 'LayoutController@indexUbahPassword']);
+//     Route::post('ubah-password', ['uses' => 'FunctionController@actionUbahPassword']);
+// });
